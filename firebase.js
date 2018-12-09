df0cef482d6245d4b4eab6daab21595c11f62f0b
Ducnm <duc2820@gmail.com>
2018-12-09 23:22:49 +0700

    init hosting firebase

--- firebase.js
+++ firebase.js
@@ -19,13 +19,9 @@ const dataBase = firebase.database()
 
 
 const users = dataBase.ref('users')
-
-
-
-
+console.log('get all id', users.push().key)
+// firebase.database().ref().child('posts').push().key;
 users.on('value', (data) => {
-    // console.log('casncjasbc', data)
-    // console.log('real time', data.val())
     document.getElementById('list').innerHTML = renderDOM(data.val())
 })
 function writeData() {
@@ -35,42 +31,56 @@ function writeData() {
     dataBase.ref('users/' + idUser).set({
         name,
         idUser,
-        email
-    })
+        email: {
+            "casncjas": {
 
+                "cascas": "cascascas",
+                "cascas": {
+                    email
+                }
+
+            }
+        }
+    })
 }
+
 function renderDOM(dataRender) {
     var html = ""
     if (dataRender && Object.keys(dataRender).length === 0) {
         console.log('ok')
         return html
     }
-
-    // console.log('datatoFireBase', dataRender)
     if (!dataRender) {
         return html
     }
     for (var item in dataRender) {
         html += `<li>${item}</li>`
-        // renderDOM(dataRender[item])
-        // console.log('value', dataRender[item])
-        // if()
         if (dataRender[item] && typeof dataRender[item] !== "string" && Object.keys(dataRender[item])[0] && Object.keys(dataRender[item]).length !== 0) {
-
             // console.log(item, dataRender[item])
             html += `<div style="margin-left : 20px">${renderDOM(dataRender[item])}</div>`
-            // console.log('string', string)
-
         }
-        //dataRender[item ] => value
-        // renderDOM(dataRender[item])
     }
-
     return html
-
-
 }
+// learn storage firebase
+// LEARN STORAGE  : CREATE , 
+const domFile = document.getElementById('file')
+const storage = firebase.storage()
+domFile.addEventListener('change', (e) => {
+    const file = e.target.files[0]
+    const storageFirebase = storage.ref('first/' + file.name)
+    console.log('dom file', e.target.files[0])
+    const task = storageFirebase.put(file)
+    task.on('state_changed', function (spanot) { console.log(spanot) }, function (err) { }, function (complete) { })
+
+})
+storage.ref('first').getDownloadURL().then(url => {
+    // const img = document.createElement('img')
 
 
-
-            // writeData("cdcbsdhjbcsbdhbcjsbdjcbjsbdchjbsd", "cascas", "cascas")
+    // Or inserted into an <img> element:
+    var img = document.getElementById('myimg');
+    img.src = url;
+    console.log('img', data)
+})
+// learn firebase function 
